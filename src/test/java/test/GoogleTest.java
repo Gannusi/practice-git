06b341db15eeb11065d0eb74d060f995272c1620
Hanna Datsko <mega.anya22@gmail.com>
2020-07-20 16:52:54 -0400

    new branch

---
+++ src/test/java/test/GoogleTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class GoogleTest {
+
+
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver= new ChromeDriver();
+        driver.get("//https:google.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public  void test1(){
+        Assert.assertEquals(driver.getTitle(), "Google");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
